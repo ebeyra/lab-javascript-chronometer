05dc4da5aaa41f3a09171d062d2b217e680506f5
Eduardo Beyra <eduardobeyra@hotmail.com>
2022-01-17 23:54:53 -0500

    Pre-work

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,37 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    setInterval(() => {
+      this.currentTime += 1
+    }, 1000)
+    return;
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = 0;
+    minutes = Math.floor(this.currentTime / 60)
+    return minutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = 0;
+    if (this.currentTime < 0) {
+      return this.currentTime
+    } else return (this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value > 9) {
+      return String(value);
+    } else return ("0" + String(value))
   }
 
   stop() {
-    // ... your code goes here
+    return clearInterval(this.intervalId)
   }
 
   reset() {
